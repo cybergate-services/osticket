b392b95da3d0ce641aaea4d6ca4d708a9df72714
Chinthaka Deshapriya <35320965+chinthakadeshapriya@users.noreply.github.com>
2019-12-19 12:11:11 +0530

    Update set-env.sh

--- set-env.sh
+++ set-env.sh
@@ -5,7 +5,7 @@ USER_PASSWORD=$(LC_ALL=C </dev/urandom tr -dc A-Za-z0-9 | head -c 28)
 DB_PASSWORD=$(LC_ALL=C </dev/urandom tr -dc A-Za-z0-9 | head -c 28)
 DB_ROOT_PASSWORD=$(LC_ALL=C </dev/urandom tr -dc A-Za-z0-9 | head -c 28)
 SECRET=$(openssl rand -hex 16)
-cat enviorenment/mariadb.env > ./.mariadb.env
+cat environment//mariadb.env > ./.mariadb.env
 cat environment/letsencrypt.env > ./.letsencrypt.env
 cat environment/osticket.env > ./.osticket.env
 
