bd4d6a39ba7b473ebf703a2514cc746f629f23b3
Chinthaka Deshapriya <35320965+chinthakadeshapriya@users.noreply.github.com>
2019-12-20 19:55:28 +0530

    Update set-env.sh

--- set-env.sh
+++ set-env.sh
@@ -7,7 +7,7 @@ DB_ROOT_PASSWORD=$(LC_ALL=C </dev/urandom tr -dc A-Za-z0-9 | head -c 28)
 SECRET=$(openssl rand -hex 16)
 cat environment/mariadb.env > ./.mariadb.env
 cat environment/letsencrypt.env > ./.letsencrypt.env
-cat environment/osticket.env > ./.osticket.env
+cat  environment/osticket.env > ./.osticket.env
 sed -ie "s/CH_MYSQL_ROOT_PASSWORD/${DB_ROOT_PASSWORD}/g" ./.mariadb.env
 sed -ie "s/CH_MYSQL_PASSWORD/${DB_PASSWORD}/g" ./.mariadb.env
 sed -ie "s/CH_MYSQL_ROOT_PASSWORD/${DB_ROOT_PASSWORD}/g" ./osticket.env
