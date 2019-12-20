06a3afe3ee4add011d359a5c8f937976cdcf7f83
Chinthaka Deshapriya <35320965+chinthakadeshapriya@users.noreply.github.com>
2019-12-20 19:51:40 +0530

    Update set-env.sh

--- set-env.sh
+++ set-env.sh
@@ -5,8 +5,12 @@ USER_PASSWORD=$(LC_ALL=C </dev/urandom tr -dc A-Za-z0-9 | head -c 28)
 DB_PASSWORD=$(LC_ALL=C </dev/urandom tr -dc A-Za-z0-9 | head -c 28)
 DB_ROOT_PASSWORD=$(LC_ALL=C </dev/urandom tr -dc A-Za-z0-9 | head -c 28)
 SECRET=$(openssl rand -hex 16)
-cat environment//mariadb.env > ./.mariadb.env
+cat environment/mariadb.env > ./.mariadb.env
 cat environment/letsencrypt.env > ./.letsencrypt.env
 cat environment/osticket.env > ./.osticket.env
-
-
+sed -ie "s/CH_MYSQL_ROOT_PASSWORD/${DB_ROOT_PASSWORD}/g" ./.mariadb.env
+sed -ie "s/CH_MYSQL_PASSWORD/${DB_PASSWORD}/g" ./.mariadb.env
+sed -ie "s/CH_MYSQL_ROOT_PASSWORD/${DB_ROOT_PASSWORD}/g" ./osticket.env
+sed -ie "s/CH_MYSQL_PASSWORD/${DB_PASSWORD}/g" ./osticket.env
+sed -ie "s/CH_INSTALL_SECRET/${SECRET}/g" ./osticket.env
+sed -ie "s/CH_ADMIN_PASSWORD/${USER_PASSWORD}/g" ./osticket.env
