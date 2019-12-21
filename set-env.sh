a25a3c849e2770fddb3d49d0224da50ef6628510
Chinthaka Deshapriya <35320965+chinthakadeshapriya@users.noreply.github.com>
2019-12-21 09:25:05 +0530

    Update set-env.sh

--- set-env.sh
+++ set-env.sh
@@ -8,9 +8,12 @@ SECRET=$(openssl rand -hex 16)
 cat environment/mariadb.env > ./.mariadb.env
 cat environment/letsencrypt.env > ./.letsencrypt.env
 cat  environment/osticket.env > ./.osticket.env
+cat  environment/db-backup.env > ./.db-backup.env
 sed -ie "s/CH_MYSQL_ROOT_PASSWORD/${DB_ROOT_PASSWORD}/g" ./.mariadb.env
 sed -ie "s/CH_MYSQL_PASSWORD/${DB_PASSWORD}/g" ./.mariadb.env
 sed -ie "s/CH_MYSQL_ROOT_PASSWORD/${DB_ROOT_PASSWORD}/g" ./.osticket.env
 sed -ie "s/CH_MYSQL_PASSWORD/${DB_PASSWORD}/g" ./.osticket.env
 sed -ie "s/CH_INSTALL_SECRET/${SECRET}/g" ./.osticket.env
 sed -ie "s/CH_ADMIN_PASSWORD/${USER_PASSWORD}/g" ./.osticket.env
+sed -ie "s/CH_DB_PASSWORD/${DB_PASSWORD}/g" ./.db-backup.env
+
